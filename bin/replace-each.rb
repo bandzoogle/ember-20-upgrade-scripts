b8bb507a9bad80e376e6653b4b37258b7ef14bb8
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-10-25 16:12:05 +0200

    This is not being used

--- bin/replace-each.rb
+++ bin/replace-each.rb
@@ -7,7 +7,6 @@ contents = File.read file_name
 IDENTIFIER = /[a-zA-Z\-\.]+/
 
 contents = contents.gsub(/\{\{#each (#{IDENTIFIER}) in (#{IDENTIFIER})/) do |match|
-  _, item, collection = Regexp.last_match
   item = Regexp.last_match[1]
   collection = Regexp.last_match[2]
 
