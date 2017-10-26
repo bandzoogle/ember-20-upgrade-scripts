b99ac180b5a08dc41a533d1e341f53ec96cbda59
Jorge Manrubia <jorge.manrubia@gmail.com>
2017-10-26 16:15:44 +0200

    Tweak script to address any attribute (not only `class`)

--- bin/replace-bind-attr.rb
+++ bin/replace-bind-attr.rb
@@ -6,8 +6,9 @@ file_name = ARGV[0]
 
 contents = File.read file_name
 
-contents = contents.gsub(/\{\{bind\-attr class=\"([^\"]+)\"\}\}/) do |match|
-  class_content = Regexp.last_match[1]
+contents = contents.gsub(/\{\{bind\-attr (#{IDENTIFIER})\s*=\s*\"([^\"]+)\"\}\}/) do |match|
+  attribute_name = Regexp.last_match[1]
+  class_content = Regexp.last_match[2]
   class_content = class_content.split(' ').collect do |segment|
     case segment
       when /(#{IDENTIFIER}):(#{IDENTIFIER}):(#{IDENTIFIER})/
@@ -21,7 +22,7 @@ contents = contents.gsub(/\{\{bind\-attr class=\"([^\"]+)\"\}\}/) do |match|
     end
   end.join(' ')
 
-  %{class="#{class_content}"}
+  %{#{attribute_name}="#{class_content}"}
 end
 
 print contents
